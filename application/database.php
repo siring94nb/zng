01be4a58c9cbb48172039fd52265926a57bde372
lilu1233 <lilusiring@163.com>
2019-08-30 16:15:33 +0800

    pull

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '',
     // 数据库名
-    'database'        => 'cabinet',
+    'database'        => '',
     // 用户名
-    'username'        => 'root',
+    'username'        => '',
     // 密码
-    'password'        => 'root',
+    'password'        => '',
     // 端口
     'hostport'        => '',
     // 连接dsn
